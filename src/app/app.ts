b51588888e75ade1c058d62a82cdc07305039d68
Hugo <hugo.lembrez@gmail.com>
2026-02-03 23:56:25 +0100

    fix: error "Home" is not use in App

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Navbar } from './shared/navbar/navbar';
-import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
