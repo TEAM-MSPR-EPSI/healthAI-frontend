28d8313bd2a2c47190ceb1c952d6da2f230a983b
Hugo <hugo.lembrez@gmail.com>
2026-05-18 15:57:06 +0200

    separate biometrics/nutrition management in a page

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -74,6 +74,23 @@ export class ApiService {
     );
   }
 
+  // --- Ingredients (admin editable) ---
+  getIngredients(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.base}/ingredients`);
+  }
+
+  updateIngredient(id: number, data: any): Observable<any> {
+    return this.http.put(`${this.base}/ingredients/${id}`, data);
+  }
+
+  createIngredient(data: any): Observable<any> {
+    return this.http.post(`${this.base}/ingredients`, data);
+  }
+
+  deleteIngredient(id: number): Observable<any> {
+    return this.http.delete(`${this.base}/ingredients/${id}`);
+  }
+
   getConsumesAll(): Observable<any[]> {
     return this.http.get<any[]>(`${this.base}/consumes`);
   }
